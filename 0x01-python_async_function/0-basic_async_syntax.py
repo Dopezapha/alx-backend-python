615d963438411e365519235fda879feccef884cd
Dopezapha <officialnwaneridaniel@gmail.com>
2024-08-06 22:39:39 +1000

    An asynchronous coroutine that takes in an integer argument named wait_random that waits for a random delay between 0 and max_delay  secs and returns it

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -2,17 +2,23 @@
 
 import asyncio
 import random
+
 """
 This module provides an asynchronous coroutine that waits for a random delay
 between 0 and a given maximum delay (inclusive) and then returns the actual
 delay waited.
 """
 
-
 async def wait_random(max_delay: int = 10) -> float:
     """
     Asynchronously waits for a random delay between 0 and max_delay seconds.
+
+    Args:
+        max_delay (int): The maximum delay in seconds. Default is 10.
+
+    Returns:
+        float: The actual delay time waited.
     """
-    delay: float = random.uniform(0, max_delay)
+    delay = random.uniform(0, max_delay)
     await asyncio.sleep(delay)
     return delay
